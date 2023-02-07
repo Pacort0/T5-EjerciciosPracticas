0c44af5df7d9381c9089b6486c8fe9cb75b9801f
Admin <Admin@DESKTOP-FNNFMRC.home>
2023-02-07 21:37:21 +0100

    comentarios funciones

--- T5_EjerciciosPracticos/src/ejercicios/funciones.java
+++ T5_EjerciciosPracticos/src/ejercicios/funciones.java
@@ -2,8 +2,16 @@ package ejercicios;
 
 public class funciones {
 
-	static final int TAMANIO = 20;
-	static final int MINAS = 6;
+	static final int TAMANIO = 20; //Variable global para definir el tamaño del tablero
+	static final int MINAS = 6; //Variable global para determinar la cantidad de minas
+	
+	/**
+	 * Se muestra el valor de la casilla elegida
+	 * @param tableroJuego es el tablero que ve el usuario
+	 * @param tableroRelleno es el tablero con las bombas y las pistas
+	 * @param casilla es el índice a destapar
+	 * @return devuelve el array 'tableroJuego' con el valor de la posición 'casilla' alterado
+	 */
 	
 	public static char[] juego(char tableroJuego[], char tableroRelleno[], int casilla) {
 		
@@ -14,6 +22,12 @@ public class funciones {
 		return tableroJuego;
 	}
 	
+	/**
+	 * Se comprueba si el jugador ha ganado o ha perdido
+	 * @param tableroRelleno tablero con las minas y las pistas
+	 * @param casilla índice a destapar
+	 * @return devuelve un booleano que dice si se ha ganado o perdido
+	 */
 	public static Boolean fin(char tableroRelleno[], int casilla) {
 		Boolean mina = false;
 		
@@ -24,45 +38,55 @@ public class funciones {
 		return mina;
 	}
 	
+	/**
+	 * Rellena el tablero de minas y pistas
+	 * @param tablero recibe el tablero vacío
+	 * @return devuelve un tablero diferente relleno
+	 */
 	public static char[] rellenaMinas(char tablero[]) {
-		int indice;
-		char tableroMinas[] = new char[TAMANIO];
-		char contador = 0;
+		int indice; //Para guardar las minas en valores aleatorios
+		char tableroMinas[] = new char[TAMANIO]; //Creamos un nuevo array 
+		char contador = 0; //Contador para rellenar las minas necesarias
 
-		for (int i = 0; i < TAMANIO; i++) {
+		for (int i = 0; i < TAMANIO; i++) { //Rellenamos el array inicialmente con ceros
 			tableroMinas[i] = '0';
 		}
 
-		while (contador < MINAS) {
+		while (contador < MINAS) { //Bucle while que se cortará tras rellenar 6 minas
 
-			indice = (int) (Math.random() * (20 + 1) - 1);
-			if (tableroMinas[indice] == '*') {
+			indice = (int) (Math.random() * (20 + 1) - 1); //Guardamos el índice generado aleatoriamente
+			if (tableroMinas[indice] == '*') { //Si en esa posición ya hay una mina, descontamos -1 al contador
 				contador--;
 			} else {
-				tableroMinas[indice] = '*';
+				tableroMinas[indice] = '*'; //Si no hay mina, la ponemos
 
-				if (bordeIzq(indice)) {
-					if (!compruebaMinaDer(tableroMinas, indice)) {
+				if (bordeIzq(indice)) { //Llamamos a la función bordeIzq
+					if (!compruebaMinaDer(tableroMinas, indice)) { //Si no hay mina a la derecha, sumamos +1 al 'indice + 1'
 						tableroMinas[indice + 1] += 1;
 					}
-				} else if (bordeDer(indice)) {
-					if (!compruebaMinaIzq(tableroMinas, indice)) {
+				} else if (bordeDer(indice)) { //Llamamos a la función bordeDer
+					if (!compruebaMinaIzq(tableroMinas, indice)) { //Si no hay mina a la izquierda, sumamos +1 a 'indice - 1'
 						tableroMinas[indice - 1] += 1;
 					}
-				} else {
-					if (!compruebaMinaIzq(tableroMinas, indice)) {
+				} else { 
+					if (!compruebaMinaIzq(tableroMinas, indice)) { //Si no hay mina a la izquierda, sumammos +1 a 'indice - 1'
 						tableroMinas[indice - 1] += 1;
 					}
-					if (!compruebaMinaDer(tableroMinas, indice)) {
+					if (!compruebaMinaDer(tableroMinas, indice)) { //Si no hay mina a la derecha, sumamos +1 a 'indice + 1'
 						tableroMinas[indice + 1] += 1;
 					}
 				}
 			}
-			contador++;
+			contador++; //Como hemos metido una mina con éxito, incrementamos el contador
 		}
-		return tableroMinas;
+		return tableroMinas; //Devolvemos el tablero relleno
 	}
-
+	
+	/**
+	 * Método para comprobar si estamos en el borde de la izquierda
+	 * @param indice recibe una posición por parámetros
+	 * @return Devuelve un booleano que nos dice si estamos en el borde o no
+	 */
 	public static Boolean bordeIzq(int indice) {
 		Boolean borde = false;
 
@@ -72,7 +96,12 @@ public class funciones {
 
 		return borde;
 	}
-
+	
+	/**
+	 * Método para comprobar si estamos en el borde de la derecha
+	 * @param indice recibe una posición por parámetros
+	 * @return Devuelve un booleano que nos dice si estamos en el borde o no
+	 */
 	public static Boolean bordeDer(int indice) {
 		Boolean borde = false;
 
@@ -81,7 +110,13 @@ public class funciones {
 		}
 		return borde;
 	}
-
+	
+	/**
+	 * Comprueba si hay una mina a la derecha de un índice dado
+	 * @param tablero recibe el tablero que está siendo rellenado
+	 * @param indice recibe una posición por parámetros
+	 * @return Devuelve un booleano que nos confirma si existe una mina o no
+	 */
 	public static Boolean compruebaMinaDer(char tablero[], int indice) {
 		Boolean mina = false;
 
@@ -91,7 +126,13 @@ public class funciones {
 
 		return mina;
 	}
-
+	
+	/**
+	 * Comprueba si hay una mina a la derecha de un índice dado
+	 * @param tablero recibe el tablero que está siendo rellenado
+	 * @param indice recibe el tablero que está siendo rellenado
+	 * @return Devuelve un booleano que nos confirma si existe una mina o no
+	 */
 	public static Boolean compruebaMinaIzq(char tablero[], int indice) {
 		Boolean mina = false;
 
